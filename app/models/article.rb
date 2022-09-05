578a9eece3421525ec05d46411b4c47458712c39
Nidhal Moussa <sinidhal2001@gmail.com>
2022-09-05 22:02:27 +0200

    CRUD tasks done exept delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,2 @@
-class Article < ActiveRecord::Base
-  validates :title, presence: true
-  validates :content, presence: true
-
-  def markdown_content
-    Kramdown::Document.new(content).to_html.html_safe
-  end
+class Article < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :totos
-
-  resources :articles
-
-  root to: 'articles#index'
+  get "/articles", to:"articles#index"
+  get "/articles/new", to:'articles#new', as: :new_article
+  post "/articles", to:'articles#create'
+  get "/articles/:id", to:"articles#show", as: :article
+  get "/articles/:id/edit", to:"articles#edit", as: :edit_article
+  patch "/articles/:id", to:"articles#update"
+  delete "/articles/:id", to:"articles#destroy"
+  # resources :articles
 end
