4d4f5d180798db78093d02f35152650273d2f9a8
Nidhal Moussa <sinidhal2001@gmail.com>
2022-09-07 20:58:29 +0200

    inplement code to solve it out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get "/articles", to:"articles#index"
-  get "/articles/new", to:'articles#new', as: :new_article
-  post "/articles", to:'articles#create'
-  get "/articles/:id", to:"articles#show", as: :article
-  get "/articles/:id/edit", to:"articles#edit", as: :edit_article
-  patch "/articles/:id", to:"articles#update"
-  delete "/articles/:id", to:"articles#destroy"
-  # resources :articles
+  resources :totos
+
+  resources :articles
+
+  root to: 'articles#index'
 end
