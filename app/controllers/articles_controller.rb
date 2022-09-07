14a74093903cf5720885554ee2ac2024caae79b2
Nidhal Moussa <102520588+Nidhal-Moussa@users.noreply.github.com>
2022-09-07 20:45:07 +0200

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,58 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:update, :show, :edit, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+  # GET /articles
   def index
     @articles = Article.all
   end
 
+  # GET /articles/1
   def show
   end
 
+  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_path
-  end
-
+  # GET /articles/1/edit
   def edit
   end
 
-  def update
-    # raise
-    @article.update(article_params)
-    redirect_to articles_path
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
   end
 
+  # PATCH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /articles/1
   def destroy
     @article.destroy
-    redirect_to restaurants_path
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
-  def article_params
-    params.require(:article).permit(:title, :content)
-  end
-
-  def find_article
-    @article = Article.find(params[:id])
-  end
+    # Only allow a trusted parameter "white list" through.
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
